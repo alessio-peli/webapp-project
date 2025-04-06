2b4b183db744c129734a97a8a5c862f5de0c558b
alessio <alessiopeli14@gmail.com>
2025-04-06 19:48:39 +0200

    creation token buttons and counter 1.0

---
+++ incremento-decremento.js
@@ -0,0 +1,22 @@
+let counter = 0;
+
+function incrementab(){
+    counter++;
+    document.getElementById("counter-bianchi").innerText = counter;
+
+}
+function decrementab(){
+    counter--;
+    document.getElementById("counter-bianchi").innerText = counter;
+    
+}
+function incrementan(){
+    counter++;
+    document.getElementById("counter-neri").innerText = counter;
+
+}
+function decrementan(){
+    counter--;
+    document.getElementById("counter-neri").innerText = counter;
+    
+}
